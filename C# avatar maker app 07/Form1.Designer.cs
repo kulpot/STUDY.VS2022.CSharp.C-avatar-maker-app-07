ced28cc879e86bc5991a4021aab13edb892c40d2
Kulpot <kulpotwahu@gmail.com>
2024-02-16 18:35:33 +0800

    ...

--- C# avatar maker app 07/Form1.Designer.cs
+++ C# avatar maker app 07/Form1.Designer.cs
@@ -479,6 +479,7 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.pictureBox1.Location = new System.Drawing.Point(443, 211);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(152, 61);
